6c9c1c4e5a5bc6632f9ba3f0769635a03eaad71d
CARLOS AROCHA <carloslarochas@gmail.com>
2021-03-30 21:06:08 +0100

    final app edition with comments

--- app/run.py
+++ app/run.py
@@ -3,102 +3,18 @@ import plotly
 import pandas as pd
 import re
 import joblib
+import sys
+sys.path.append('../models')
+from train_classifier import OrganizationPresence, TextLengthExtractor
+from train_classifier import tokenize
 
 from sqlalchemy import create_engine
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-from nltk import ne_chunk
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.stem.porter import PorterStemmer
-from nltk import tree2conlltags
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 
 
 app = Flask(__name__)
-
-class OrganizationPresence(BaseEstimator, TransformerMixin):
-
-    def checking_org(self, text):
-        #
-        words = word_tokenize(text)
-        words = [w for w in words if w.lower() not in stopwords.words('english')]
-
-        ptree = pos_tag(words)
-
-        for w in tree2conlltags(ne_chunk(ptree)):
-            if (w[2][2:] == 'ORGANIZATION') and (w[1] == 'NNP'):
-                return True
-
-            return False
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, X):
-        #
-        X_org = pd.Series(X).apply(self.checking_org)
-
-        return pd.DataFrame(X_org)
-
-class TextLengthExtractor(BaseEstimator, TransformerMixin):
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        #
-        return pd.DataFrame(pd.Series(X).apply(lambda x: len(x)))
-
-def tokenize(text):
-    # Changing every webpage for a space.
-    # With this regex we delete webpages with these characteristics:
-    #       1. http://www.name.ext or similar
-    #       2. http : www.name.ext or similar
-    #       3. http www.name.ext or similar
-    url_regex = 'http[s]?[\s]?[:]?[\s]?[\/\/]?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-
-    # Then clean the texts of webpages addresses
-    detected_urls = re.findall(url_regex, text)
-    for url in detected_urls:
-        text = text.replace(url, " ")
-
-    # Forgetting about the numbers and any non letter char
-    text = re.sub('[^a-zA-Z]',' ',text)
-
-    tokens = word_tokenize(text)
-
-    tags = {"J": wordnet.ADJ,
-            "N": wordnet.NOUN,
-            "V": wordnet.VERB,
-            "R": wordnet.ADV}
-
-    particular_words = ['kg']
-    total_stopwords = particular_words + stopwords.words('english')
-
-    lemmatizer = WordNetLemmatizer()
-    stemmer = PorterStemmer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok, \
-                tags.get(pos_tag([tok])[0][1][0].upper(), wordnet.NOUN)).lower()
-        clean_tok = stemmer.stem(clean_tok)
-        if clean_tok not in total_stopwords:
-            clean_tokens.append(clean_tok)
-
-    return  clean_tokens
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
@@ -119,6 +35,7 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
 
+    # Now define a list with the categories to be count and graph.
     category_names = ['related', 'request', 'offer',
        'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
        'security', 'military', 'water', 'food', 'shelter', 'clothing', 'money',
@@ -127,7 +44,7 @@ def index():
        'tools', 'hospitals', 'shops', 'aid_centers', 'other_infrastructure',
        'weather_related', 'floods', 'storm', 'fire', 'earthquake', 'cold',
        'other_weather', 'direct_report', 'not_related']
-
+    # Second graph counting the related categories in total
     category_counts = df[category_names].sum()
     category_names = [re.sub('_',' ', text) for text in category_names]
 
@@ -159,7 +76,8 @@ def index():
                     y=category_counts
                 )
             ],
-
+            # For the second graph we repeat the same structure with some
+            # particular attributes.
             'layout': {
                 'title': 'Distribution of Messages by Category',
                 'yaxis': {
@@ -203,57 +121,9 @@ def go():
         classification_result=classification_results
     )
 
-@app.route('/graph')
-def graph():
-    # save user input in query
-    query = request.args.get('query', '')
-
-    # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
-
-    # This will render the go.html Please see that file.
-    return render_template(
-        'graph.html',
-        query=query,
-        classification_result=classification_results
-    )
-
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
 
 
 if __name__ == '__main__':
     main()
-
-
-'''
-# extract data needed for visuals
-# TODO: Below is an example - modify to extract data for your own visuals
-genre_counts = df.groupby('genre').count()['message']
-genre_names = list(genre_counts.index)
-
-# create visuals
-# TODO: Below is an example - modify to create your own visuals
-graphs = [
-    {
-        'data': [
-            Bar(
-                x=genre_names,
-                y=genre_counts
-            )
-        ],
-
-        'layout': {
-            'title': 'Distribution of Message Genres',
-            'yaxis': {
-                'title': "Count"
-            },
-            'xaxis': {
-                'title': "Genre"
-            }
-        }
-    }
-]
-
-'''
